de85981f37f6ebe068153a95738ba8e13ea1834e
eric <ericlam@vitessedata.com>
2023-07-12 15:57:52 +0100

    update xrg-r230712

--- release.sh
+++ release.sh
@@ -1,13 +1,20 @@
-if [ -z ${1+x} ]; then
-    echo "usage: $0 version"; echo
+if [ $# -eq 0 ]; then
+    echo "usage: $0 version [branch]"; echo
     exit 1
 fi
 
+BRANCH="main"
 VERSION=$1
 KITESDK=kite-client-sdk-$VERSION
+if [ $# -eq 2 ]; then
+BRANCH=$2
+echo "BRANCH=$BRANCH"
+fi
+
 echo $KITESDK
-rm -rf $KITESDK $EXPREVAL.zip
+rm -rf $KITESDK $KITESDK.zip
 git clone git@github.com:vderic/kite-client-sdk.git $KITESDK
+(cd $KITESDK && git checkout $BRANCH )
 find $KITESDK -name '*.xrg' -delete
 find $KITESDK -name '*.csv' -delete
 find $KITESDK -name '*.csv.gz' -delete
