785cfc3040ba7606afb52f7ec077c6346d88bd3c
eric <ericlam@vitessedata.com>
2023-12-22 19:19:42 +0000

    setup.py

---
+++ python/setup.py
@@ -0,0 +1,15 @@
+# encoding: UTF-8
+
+from distutils.core import setup
+
+setup(
+    name='kite',
+    version='0.1.0',
+    author='Eric Lam',
+    author_email='ericlam@vitessedata.com',
+    license='Apache License 2.0',
+    url='https://github.com/vderic/kite-client-sdk',
+    description='Kite Client for Python',
+    packages=[],
+    py_modules=['kite'],
+)
